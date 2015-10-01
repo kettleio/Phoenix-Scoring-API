36d038ad82d406da5518b1f7b78babcae63f622e
Drew Witmer <drew@kettle.io>
2015-10-01 11:49:11 -0700

    Routes + MVC structure

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,14 @@
-defmodule KettlePhoenix.Router do
+defmodule KettlePhoenix.Router do  
   use KettlePhoenix.Web, :router
-
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", KettlePhoenix do
-    pipe_through :browser # Use the default browser stack
+  scope "/api", KettlePhoenix do
+    pipe_through :api
 
-    get "/", PageController, :index
-  end
+    resources "/scores", ScoreController, except: [:new, :edit]
+  end 
 
-  # Other scopes may use custom stacks.
-  # scope "/api", KettlePhoenix do
-  #   pipe_through :api
-  # end
-end
+end 
